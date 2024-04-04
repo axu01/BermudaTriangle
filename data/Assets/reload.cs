ed513f7e29cc40655dce1a3c7d930afc3ef9f4f9
axu01 <angyna2415@gmail.com>
2024-04-04 11:41:24 -0400

    initial commit
    contains assets, packages, project settings and user settings
    
    -project was created with 2d URP

---
+++ data/Assets/reload.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class reload : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollision2DEnter(Collision2D collision)
+    {
+        //if (col.gameObject.tag == “Birb”)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
