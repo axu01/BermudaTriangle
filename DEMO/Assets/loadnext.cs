c62c0e552bc853758cd23f4aced2bf415aa06d89
axu01 <angyna2415@gmail.com>
2024-04-11 10:21:30 -0400

    sound cave thing

--- DEMO/Assets/loadnext.cs
+++ DEMO/Assets/loadnext.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class loadnext : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void OnCollisionEnter2D(Collision2D col)
     {
-        
+        SceneManager.LoadScene(2);
     }
 
     // Update is called once per frame
