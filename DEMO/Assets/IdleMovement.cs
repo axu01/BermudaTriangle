cf91b17fa4fe2d0102ec1a0abf8837188d3bc55e
axu01 <angyna2415@gmail.com>
2024-06-25 14:03:06 -0400

    6/25/2024
    Added a few more mechanics to the 7th scene and added a scene transition into text screen.

---
+++ DEMO/Assets/IdleMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
+public class IdleMovement : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    //public GameObject birb;
+    public float movSpeed = 10.4f;
+    float speedX, speedY;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(100f, 0f) * Time.deltaTime);
+        //Vector3 pos = transform.position;
+        //speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+        //speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        //rb.velocity = new Vector2(speedX, speedY);
+
+    }
+}
